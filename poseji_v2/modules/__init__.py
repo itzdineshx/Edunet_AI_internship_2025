baa329a2517a83fba3fef2f2fe6e59c655c1e74d
DINESH S <140992234+itzdineshx@users.noreply.github.com>
2025-04-06 08:53:33 +0530

    Update __init__.py

--- poseji_v2/modules/__init__.py
+++ poseji_v2/modules/__init__.py
@@ -1,10 +1,21 @@
-# poseji/modules/__init__.py
-
 from .config import set_config, inject_custom_css
-from .helpers import calculate_angle, calculate_biomechanics_mediapipe, calculate_biomechanics_movenet
-from .pose_estimators import get_pose_analyzer, OpenPoseAnalyzer, MediaPipePoseAnalyzer, MoveNetAnalyzer
+from .helpers import (
+    calculate_angle,
+    calculate_biomechanics_mediapipe,
+    calculate_biomechanics_movenet,
+)
+from .pose_estimators import (
+    get_pose_analyzer,
+    OpenPoseAnalyzer,
+    MediaPipePoseAnalyzer,
+    MoveNetAnalyzer,
+)
 from .video_estimation import run_video_estimation, generate_report, save_session
-from .webcam_transformers import WebcamPoseTransformer, WebcamPostureFeedbackTransformer, ExerciseAnalysisTransformer
+from .webcam_transformers import (
+    WebcamPoseTransformer,
+    WebcamPostureFeedbackTransformer,
+    ExerciseAnalysisTransformer,
+)
 from .image_analysis import run_image_analysis
 from .session_history import display_session_history
 from .main_ui import main
